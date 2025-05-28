c24b0d09913e3d155227979611bd1632aaff61e3
OtakiHaku <te220276otaki@gmail.com>
2025-05-28 12:48:47 +0900

    dekitaa

--- src/test/java/com/example/mybootapp8/Mybootapp8ApplicationTests.java
+++ src/test/java/com/example/mybootapp8/Mybootapp8ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.mybootapp7;
+package com.example.mybootapp8;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Mybootapp7ApplicationTests {
+class Mybootapp8ApplicationTests {
 
 	@Test
 	void contextLoads() {
